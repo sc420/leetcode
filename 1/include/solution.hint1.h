adcd3223f887fa3426293b076cc1a8542b88ac6a
Shawn Chang <ebola777gm@gmail.com>
2021-04-27 15:59:50 +0800

    add scripts

--- 1/include/solution.hint1.h
+++ 1/include/solution.hint1.h
@@ -2,9 +2,10 @@ class Solution {
  public:
   std::vector<int> twoSum(std::vector<int>& nums, int target) {
     for (size_t i = 0; i < nums.size(); i++) {
+      const int num1 = nums[i];
       for (size_t j = 0; j < nums.size(); j++) {
         if (i == j) continue;
-        if (nums[i] + nums[j] == target) return {(int)i, (int)j};
+        if (num1 + nums[j] == target) return {(int)i, (int)j};
       }
     }
     return {};  // Should not happen
