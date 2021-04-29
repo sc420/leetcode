48a67f381191f91c21a76d1d825068a965f68f27
Shawn Chang <ebola777gm@gmail.com>
2021-04-29 22:13:29 +0800

    add solution for #2

---
+++ 2/include/solution.h
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode *nodes[10];
+    for (int i = 0; i < 10; i++) {
+      nodes[i] = new ListNode(i);
+    }
+    ListNode *head = nullptr;
+    ListNode *prevNode = nullptr;
+    int carry = 0;
+    while (true) {
+      const int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+      carry = sum / 10;
+      curNode->val = sum % 10;
+      l1 = l1 ? l1->next : l1;
+      l2 = l2 ? l2->next : l2;
+      if (!l1 && !l2 && !carry) {
+        break;
+      }
+      curNode->next = new ListNode();
+      curNode = curNode->next;
+    }
+    delete curNode->next;
+    curNode->next = nullptr;
+    return head;
+  }
+};
